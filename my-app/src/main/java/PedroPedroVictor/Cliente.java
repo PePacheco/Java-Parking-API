427a16cae781f892bf0e66d88e955f4affea0fce
PePacheco <pedro_pacheco98@outlook.com>
2020-06-19 17:49:54 -0300

    Ajustando as classes das entidades

--- my-app/src/main/java/PedroPedroVictor/Cliente.java
+++ my-app/src/main/java/PedroPedroVictor/Cliente.java
@@ -3,14 +3,33 @@ package PedroPedroVictor;
 import java.util.ArrayList;
 
 public class Cliente {
+    private Sexo sexo;
     private int rg;
     private int qtdadeDeUsos;
-    private ArrayList<Integer> vagasPreferidas;
+    private ArrayList<Carro> carros;
+    private ArrayList<Vaga> vagasPreferidas;
 
-    public Cliente(int rg) {
+    public Cliente(int rg, Sexo s) {
         this.rg = rg;
+        this.sexo = s;
         this.qtdadeDeUsos = 0;
-        vagasPreferidas = new ArrayList<Integer>();
+        vagasPreferidas = new ArrayList<Vaga>();
+    }
+
+    public void cadastrarCarro(Carro c) {
+        carros.add(c);
+    }
+
+    public ArrayList<Vaga> getVagasPreferidas() {
+        return this.vagasPreferidas;
+    }
+
+    public Sexo getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(Sexo s) {
+        this.sexo = s;
     }
 
     public int getRg() {
