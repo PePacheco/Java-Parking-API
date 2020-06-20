62e021df4eeab9c7ce96ec5aea511de465c766eb
VitaoDoEspadao <victor.huander@outlook.com>
2020-06-19 22:13:32 -0300

    adicionando Classes De Teste

---
+++ my-app/src/test/java/PedroPedroVictor/ClienteTest.java
@@ -0,0 +1,70 @@
+package PedroPedroVictor;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ClienteTest
+{
+
+    @Test
+    public void estacionaSemCarro() {
+        Vaga v = new Vaga(1);
+        Cliente c = new Cliente(123456, Sexo.MASCULINO);
+        assertTrue( !c.estacionar(v) );
+    }
+
+    @Test
+    public void estacionaComCarroVagaOcupada() {
+        Vaga v = new Vaga(1);
+        v.ocupa();
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Cliente cliente = new Cliente(123456, Sexo.MASCULINO);
+        cliente.adicionarCarro(c);
+        assertTrue( !cliente.estacionar(v) );
+    }
+
+    @Test
+    public void estacionaComCarroVagaDesocupada() {
+        Vaga v = new Vaga(1);
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Cliente cliente = new Cliente(123456, Sexo.MASCULINO);
+        cliente.adicionarCarro(c);
+        assertTrue( cliente.estacionar(v) );
+    }
+
+    @Test
+    public void atualizaVagasPreferidas() {
+        Vaga v1 = new Vaga(1);
+        Vaga v2 = new Vaga(2);
+        Vaga v3 = new Vaga(3);
+        Vaga v4 = new Vaga(4);
+
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Cliente cliente = new Cliente(123456, Sexo.MASCULINO);
+        cliente.adicionarCarro(c);
+
+        cliente.estacionar(v1);
+        v1.retirarCarro();
+        cliente.estacionar(v1);
+        v1.retirarCarro();
+        cliente.estacionar(v1);
+        v1.retirarCarro();
+        cliente.estacionar(v2);
+        v2.retirarCarro();
+        cliente.estacionar(v3);
+        v3.retirarCarro();
+        cliente.estacionar(v4);
+        v4.retirarCarro();
+        cliente.estacionar(v4);
+        v4.retirarCarro();
+
+        if(cliente.getVagasPreferidas().get(0).getPosicao() == 1) {
+            assertTrue( true );
+        } else {
+            assertTrue( false );
+        }
+    }
+}
---
+++ my-app/src/test/java/PedroPedroVictor/VagaTest.java
@@ -0,0 +1,54 @@
+package PedroPedroVictor;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class VagaTest {
+    @Test
+    public void estacionarVagaOcupada() {
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Vaga v = new Vaga(1);
+        v.ocupa();
+        assertTrue( !v.estacionarCarro(c) );
+    }
+
+    @Test
+    public void estacionarVagaDesocupada() {
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Vaga v = new Vaga(1);
+        assertTrue( v.estacionarCarro(c) );
+    }
+
+    @Test
+    public void estacionarVagaAposDesocupar() {
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Vaga v = new Vaga(1);
+        v.ocupa();
+        v.desocupa();
+        assertTrue( v.estacionarCarro(c) );
+    }
+
+    @Test
+    public void retirarVagaDesocupada() {
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Vaga v = new Vaga(1);
+        assertTrue( !v.retirarCarro() );
+    }
+
+    @Test
+    public void retirarVagaOcupada() {
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Vaga v = new Vaga(1);
+        v.estacionarCarro(c);
+        assertTrue( v.retirarCarro() );
+    }
+
+    @Test
+    public void retirarVagaAposDesocupar() {
+        Carro c = new Carro("azul", "Fiat", "ABC123");
+        Vaga v = new Vaga(1);
+        v.estacionarCarro(c);
+        v.desocupa();
+        assertTrue( !v.retirarCarro() );
+    }
+}
